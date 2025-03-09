6ca0716275ea8e26b58c69cac4cb93693817fb7a
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-03-09 16:48:15 +0530

    Added: API data get.

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -4,3 +4,8 @@ export const getWeatherData = async (city: string | null) => {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`);
     return response;
 }
+
+export const get5DaysWeather = async (query: string | null) => {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`);
+    return response;
+}
